f5953c207382c35b8230d5b8ca5c63cd9a18da31
Adao <sansonov@gmail.com>
2020-06-07 17:13:36 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addList)
+
+
+  function addList(e) {
+    e.preventDefault();
+    const taskList = document.getElementById('tasks')
+    const list = document.createElement('li');
+    const priority = e.target[1].value
+    list.textContent = e.target[0].value
+    if (priority == 'red') { list.style.color = 'red'};
+    if (priority == 'yellow') { list.style.color = 'yellow'};
+    if (priority == 'green') { list.style.color = 'green'};
+    // debugger
+    taskList.append(list);
+    document.getElementById('create-task-form').reset();
+    list.addEventListener('click', function (e) {
+      e.target.remove();
+    }) 
+
+  }
+ 
+  
 });
+
+
